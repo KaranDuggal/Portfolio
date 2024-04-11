05337d21ddcf0befc495d41ae0a9b56bd03b405a
Karan Duggal <karan.duggal@dazn.com>
2024-04-11 20:10:43 +0530

    feat: skill section add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
   public isCollapsed = true;
+  skills:Array<String> = ['Angular', 'NodeJS', 'ExpressJS', 'NestJs ', 'Socket.io', 'RXJS', 'Postgres', 'MySql', 'MongoDB', 'JavaScript', 'TypeScript', 'Flutter', 'GIT', 'Jira', 'AWS Data Streams', 'AWS', 'Lambda', 'DynamoDB', 'RDS', 'Docker', 'Microsoft Azure', 'DSA', 'Selenium - webdriver']
 }
