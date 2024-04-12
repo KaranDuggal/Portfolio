a62e59d805f477a69d4ab4a6de098abd100482ad
Karan Duggal <karan.duggal@dazn.com>
2024-04-12 14:05:43 +0530

    feat: nav bar design

--- src/app/component/common/nav/nav.component.ts
+++ src/app/component/common/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-
+  isSidebarVisible:boolean = false;
+  changeSidebarStatus(){
+    this.isSidebarVisible = !this.isSidebarVisible
+  }
 }
