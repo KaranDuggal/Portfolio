3184b50f9ec48c1fc41214c20a98098e0fd0e5c6
Karan <duggalkaran400@gmail.com>
2024-08-01 13:10:10 +0530

    feat: skill add

--- src/app/component/core/home/skill-section/skill-section.component.ts
+++ src/app/component/core/home/skill-section/skill-section.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './skill-section.component.scss'
 })
 export class SkillSectionComponent {
-  skills:Array<string> = ['Angular', 'NodeJS', 'ExpressJS', 'NestJs ', 'Socket.io', 'RXJS', 'Postgres', 'MySql', 'MongoDB', 'JavaScript', 'TypeScript', 'Flutter', 'GIT', 'Jira', 'AWS Data Streams', 'AWS', 'Lambda', 'DynamoDB', 'RDS', 'Docker', 'Microsoft Azure', 'DSA', 'Selenium - webdriver']
+  skills:Array<string> = ['Angular', 'NodeJS', 'ExpressJS', 'NestJs ', 'Socket.io', 'RXJS', 'Postgres', 'MySql', 'MongoDB', 'JavaScript', 'TypeScript', 'Flutter', 'GIT', 'Jira', 'AWS Data Streams', 'AWS', 'Lambda', 'DynamoDB', 'SQS', 'DLQ', 'RDS', 'Docker', 'Microsoft Azure', 'Terraform', 'DSA', 'Selenium - webdriver']
 
 }
